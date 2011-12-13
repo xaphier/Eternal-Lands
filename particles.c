3a2b404f37954602750c92ef67455f109bff27a9
Xaphier <el.3d.source@googlemail.com>
2011-12-13 19:38:21 +0100

    * Particle & eye-candy fix.

--- particles.c
+++ particles.c
@@ -23,14 +23,11 @@
 #include "translate.h"
 #include "io/elpathwrapper.h"
 #include "io/elfilewrapper.h"
-#ifdef MAP_EDITOR
-#include "map_editor/lights.h"
-#else
 #include "lights.h"
-#endif
 #ifdef	NEW_TEXTURES
 #include "image_loading.h"
 #endif	/* NEW_TEXTURES */
+#include "engine.h"
 
 /* NOTE: This file contains implementations of the following, currently unused, and commented functions:
  *          Look at the end of the file.
@@ -61,82 +58,46 @@ int use_point_particles = 1;
 #else
 int use_point_particles = 0;
 #endif
-int particles_percentage=100;
 int enable_blood = 0;
+
+/******************************************************
+ *           PARTICLE SYSTEM DEFINITIONS              *
+ ******************************************************/
+
+int part_strcmp(char * s1, char *s2)
+{
+	while(*s1 && *s2)
+		{
+			if(*s1!=*s2 && (*s1!='/' && *s1!='\\' && *s2!='/' && *s2!='\\' )) return 1;
+			s1++;s2++;
+		}
+	return *s1!=*s2;
+}
+
+static __inline__ void calc_particle_random_min_max(float f1, float f2, float* v_min, float* v_max)
+{
+	if (f1 < f2)
+	{
+		*v_min = f1;
+		*v_max = f2;
+	}
+	else
+	{
+		*v_min = f2;
+		*v_max = f1;
+	}
+}
+
+static __inline__ void calc_particle_random2_min_max(float f1, float f2, float* v_min, float* v_max)
+{	
+	*v_min = (f1+f2)*0.5f-abs(f2-f1);
+	*v_max = (f1+f2)*0.5f+abs(f2-f1);
+}
 
 #ifdef MAP_EDITOR2
 #define MAP_EDITOR
 #endif
 
-#ifdef MAP_EDITOR
-int save_particle_def(particle_sys_def *def)
-{
-	char cleanpath[128];
-	FILE *f=NULL;
-
-	clean_file_name(cleanpath, def->file_name, sizeof(cleanpath));
-
-	f=open_file_data(cleanpath,"w");
-	if(f == NULL){
-		LOG_ERROR("%s: %s \"%s\"\n", reg_error_str, cant_open_file, cleanpath);
-		return 0;
-	}
-
-	fprintf(f,"%i\n",PARTICLE_DEF_VERSION);
-
-	// System info
-	fprintf(f,"%i\n",def->part_sys_type);
-	fprintf(f,"%x,%x\n",def->sblend,def->dblend);
-	fprintf(f,"%i\n",def->total_particle_no);
-	fprintf(f,"%i\n",def->ttl);
-	fprintf(f,"%i\n",def->part_texture);
-	fprintf(f,"%f\n",def->part_size);
-	fprintf(f,"%i\n",def->random_func);
-	// Particle creation info
-	fprintf(f,"%f,%f,%f\n",def->minx,def->miny,def->minz);
-	fprintf(f,"%f,%f,%f\n",def->maxx,def->maxy,def->maxz);
-	fprintf(f,"%f\n",def->constrain_rad_sq);
-	fprintf(f,"%f,%f,%f\n",def->vel_minx,def->vel_miny,def->vel_minz);
-	fprintf(f,"%f,%f,%f\n",def->vel_maxx,def->vel_maxy,def->vel_maxz);
-	fprintf(f,"%f,%f,%f,%f\n",def->minr,def->ming,def->minb,def->mina);
-	fprintf(f,"%f,%f,%f,%f\n",def->maxr,def->maxg,def->maxb,def->maxa);
-	// Particle update info
-	fprintf(f,"%f,%f,%f\n",def->acc_minx,def->acc_miny,def->acc_minz);
-	fprintf(f,"%f,%f,%f\n",def->acc_maxx,def->acc_maxy,def->acc_maxz);
-	fprintf(f,"%f,%f,%f,%f\n",def->mindr,def->mindg,def->mindb,def->minda);
-	fprintf(f,"%f,%f,%f,%f\n",def->maxdr,def->maxdg,def->maxdb,def->maxda);
-	// Particle light info
-	fprintf(f,"%i\n",def->use_light);
-	fprintf(f,"%f,%f,%f\n",def->lightx,def->lighty,def->lightz);
-	fprintf(f,"%f,%f,%f\n",def->lightr,def->lightg,def->lightb);
-
-	fclose(f);
-	return 1;
-}
-#endif
-#ifdef MAP_EDITOR2
-#undef MAP_EDITOR
-#endif
-
-/*******************************************************************
- *            INITIALIZATION AND CLEANUP FUNCTIONS                 *
- *******************************************************************/
-//Threading support for particles_list
-void init_particles ()
-{
-}
-
-void end_particles ()
-{
-}
-
-void destroy_all_particle_defs()
-{
-}
-
-void destroy_all_particles()
-{
-}
 #ifndef MAP_EDITOR
 void add_fire_at_tile (int kind, Uint16 x_tile, Uint16 y_tile)
 {
@@ -306,6 +267,61 @@ int add_particle_sys (const char *file_name, float x_pos, float y_pos, float z_p
 int add_particle_sys (const char *file_name, float x_pos, float y_pos, float z_pos)
 #endif
 {
+	if (use_eye_candy)
+	{
+		if (!strncmp("fou", file_name + 12, 3))
+			ec_create_fountain(x_pos, y_pos, z_pos + 0.15, 0.0, 1.0, (z_pos >= 0.8 ? z_pos - 0.8 : 0.0), 0, 1.0, (poor_man ? 6 : 10));
+		else if ((use_fancy_smoke) && (!strncmp("smo", file_name + 12, 3)))
+		{
+			if (file_name[17] == '1')
+				ec_create_smoke(x_pos, y_pos, z_pos, 0.0, 1.0, 0.3, (poor_man ? 6 : 10));
+			else if (file_name[17] == '2')
+				ec_create_smoke(x_pos, y_pos, z_pos, 0.0, 1.0, 0.45, (poor_man ? 6 : 10));
+			else if (file_name[17] == '3')
+				ec_create_smoke(x_pos, y_pos, z_pos, 0.0, 1.0, 1.6, (poor_man ? 6 : 10));
+			else if (file_name[17] == '_')
+				ec_create_smoke(x_pos, y_pos, z_pos, 0.0, 1.0, 1.1, (poor_man ? 6 : 10));
+			else
+				ec_create_smoke(x_pos, y_pos, z_pos, 0.0, 1.0, 0.5, (poor_man ? 6 : 10));
+		}
+		else if (!strncmp("tel", file_name + 12, 3))
+		{
+			if (file_name[21] == 'i')
+			{
+				ec_create_selfmagic_teleport_to_the_portals_room(x_pos, y_pos, z_pos, (poor_man ? 6 : 10));
+			}
+			else if (file_name[21] == 'o')
+			{
+				ec_create_selfmagic_teleport_to_the_portals_room(x_pos, y_pos, z_pos, (poor_man ? 6 : 10));
+			}
+			else
+				ec_create_teleporter(x_pos, y_pos, z_pos, 0.0, 1.0, 1.0, (poor_man ? 6 : 10));
+		}
+		else if (!strncmp("fir", file_name + 12, 3))
+		{
+			if (!strncmp("big", file_name + 17, 3))
+				ec_create_campfire(x_pos, y_pos, z_pos, 0.0, 1.0, (poor_man ? 6 : 10), 1.5);
+			else if (!strncmp("for", file_name + 17, 3))
+				ec_create_campfire(x_pos, y_pos - 0.2, z_pos, 0.0, 1.0, (poor_man ? 6 : 10), 2.0);
+			else if (!strncmp("min", file_name + 17, 3))
+				ec_create_campfire(x_pos, y_pos, z_pos, 0.0, 1.0, (poor_man ? 6 : 10), 0.4);
+			else if (!strncmp("sma", file_name + 17, 3))
+				ec_create_campfire(x_pos, y_pos, z_pos, 0.0, 1.0, (poor_man ? 6 : 10), 0.6);
+			else if (!strncmp("tor", file_name + 17, 3))
+				ec_create_lamp(x_pos, y_pos, z_pos, 0.0, 1.0, 1.6, (poor_man ? 6 : 10));
+			else
+			{
+				return -1;
+			}
+		}
+		else if (!strncmp("can", file_name + 12, 3))
+			ec_create_candle(x_pos, y_pos, z_pos, 0.0, 1.0, 0.7, (poor_man ? 6 : 10));
+		else
+		{
+				return -1;
+		}
+	}
+
 	// If we got here, the eye candy system handled this particle
 	// system. Return an invalid particle ID to signal that nothing
 	// was added to particles_list, but not -1 since this is not an 
@@ -354,25 +370,81 @@ int add_particle_sys_at_tile (const char *file_name, int x_tile, int y_tile)
 #endif
 }
 
-#ifndef ELC
-int have_point_sprite=0;
-#endif
-
-void display_particles()
-{
-}
-
-void update_particles()
-{
-}
-
 /******************************************************************************
  *                        MISC HELPER FUNCTIONS                               *
  ******************************************************************************/
 #ifdef ELC
 void add_teleporters_from_list (const Uint8 *teleport_list)
 {
+	Uint16 teleporters_no;
+	int i;
+	int teleport_x,teleport_y,my_offset;
+	float x,y,z;
+
+	teleporters_no=SDL_SwapLE16(*((Uint16 *)(teleport_list)));
+
+	for (i = 0; i < teleporters_no; i++)
+	{
+		my_offset = i * 5 + 2;
+		teleport_x=SDL_SwapLE16(*((Uint16 *)(teleport_list+my_offset)));
+		teleport_y=SDL_SwapLE16(*((Uint16 *)(teleport_list+my_offset+2)));
+						
+		//later on, maybe we want to have different visual types
+		//now, get the Z position
+		if(teleport_y*tile_map_size_x*6+teleport_x<tile_map_size_y*6*tile_map_size_x*6)
+			z=-2.2f+height_map[teleport_y*tile_map_size_x*6+teleport_x]*0.2f;
+		else continue;
+		//convert from height values to meters
+		x=(float)teleport_x/2;
+		y=(float)teleport_y/2;
+		//center the object
+		x += 0.25f;
+		y += 0.25f;
+
+		add_particle_sys ("./particles/teleporter.part", x, y, z, 1);
+		add_object_engine("./3dobjects/portal1.e3d", x, y, z, 0.0f,
+			0.0f, 0.0f, 0, 1.0f, 1.0f, 1.0f, get_next_free_id(),
+			st_detect);
+			
+		//mark the teleporter as an unwalkable so that the pathfinder
+		//won't try to plot a path through it
+
+		pf_tile_map[teleport_y*tile_map_size_x*6+teleport_x].z = 0;
+	}
 }
+
+/* currently UNUSED
+void dump_part_sys_info()
+{
+	char str[256];
+	int i,partdefs=0,partsys=0;
+	LOG_TO_CONSOLE(c_grey1,particle_system_dump);
+	if(!particles_percentage)
+		{
+			LOG_TO_CONSOLE(c_grey1,particles_disabled_str);
+			return;
+		}
+	if(have_point_sprite)
+	  LOG_TO_CONSOLE(c_grey1,point_sprites_enabled);
+	else
+	  LOG_TO_CONSOLE(c_grey1,using_textured_quads);
+	LOG_TO_CONSOLE(c_grey1,definitions_str);
+	for(i=0;i<MAX_PARTICLE_DEFS;i++)
+		if(defs_list[i])
+			{
+				partdefs++;
+				LOG_TO_CONSOLE(c_grey1,defs_list[i]->file_name);
+			}
+	safe_snprintf(str, sizeof(str), "#%s: %i",my_tolower(definitions_str),partdefs);
+	LOG_TO_CONSOLE(c_grey1,str);
+	for(i=0;i<MAX_PARTICLE_SYSTEMS;i++)
+		if(particles_list[i])partsys++;
+	safe_snprintf(str, sizeof(str), "#%s: %i",part_sys_str,partsys);
+	LOG_TO_CONSOLE(c_grey1,str);
+	safe_snprintf(str, sizeof(str), "#%s: %i%%",part_part_str,particles_percentage);
+	LOG_TO_CONSOLE(c_grey1,str);
+}
+*/
 #endif // ELC
 
 #ifdef MAP_EDITOR
@@ -385,4 +457,3 @@ void get_and_set_particle_texture_id (int i)
 #endif	/* NEW_TEXTURES */
 }
 #endif // MAP_EDITOR
-
