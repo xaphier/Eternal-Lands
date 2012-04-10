aa8572b08eff9773a8e3d2bac21323acbfb93f16
Xaphier <el.3d.source@googlemail.com>
2012-04-10 11:16:06 +0200

    * Simplified vector and matrix stream in & out.  * Added tests for vector and matrix stream in & out.

--- engine/glmutil.cpp
+++ engine/glmutil.cpp
@@ -10,510 +10,376 @@
 namespace eternal_lands
 {
 
+	namespace
+	{
+
+		template <typename T>
+		OutStream& print_quat(OutStream &str, const T &value)
+		{
+			Uint16 i;
+
+			str << value[0];
+
+			for (i = 1; i < 4; ++i)
+			{
+				str << UTF8(" ") << value[i];
+			}
+
+			return str;
+		}
+
+		template <typename T>
+		OutStream& print_vec(OutStream &str, const T &value)
+		{
+			Uint16 i;
+
+			str << value[0];
+
+			for (i = 1; i < value.length(); ++i)
+			{
+				str << UTF8(" ") << value[i];
+			}
+
+			return str;
+		}
+
+		template <typename T>
+		OutStream& print_bvec(OutStream &str, const T &value)
+		{
+			Uint16 i;
+
+			if (value[0])
+			{
+				str << UTF8("true");
+			}
+			else
+			{
+				str << UTF8("false");
+			}
+
+			for (i = 1; i < value.length(); ++i)
+			{
+				if (value[i])
+				{
+					str << UTF8(" true");
+				}
+				else
+				{
+					str << UTF8(" false");
+				}
+			}
+
+			return str;
+		}
+
+		template <typename T>
+		OutStream& print_mat(OutStream &str, const T &value)
+		{
+			Uint16 i, j;
+
+			for (i = 0; i < value.row_size(); ++i)
+			{
+				for (j = 0; j < value.col_size(); ++j)
+				{
+					if ((i > 0) || (j > 0))
+					{
+						str << UTF8(" ");
+					}
+
+					str << value[i][j];
+				}
+			}
+
+			return str;
+		}
+
+		template <typename T>
+		InStream& scan_quat(InStream &str, T &value)
+		{
+			Uint16 i;
+
+			for (i = 0; i < 4; ++i)
+			{
+				str >> value[i];
+			}
+
+			return str;
+		}
+
+		template <typename T>
+		InStream& scan_vec(InStream &str, T &value)
+		{
+			Uint16 i;
+
+			for (i = 0; i < value.length(); ++i)
+			{
+				str >> value[i];
+			}
+
+			return str;
+		}
+
+		template <typename T>
+		InStream& scan_bvec(InStream &str, T &value)
+		{
+			std::string tmp;
+			Uint16 i;
+
+			for (i = 0; i < value.length(); ++i)
+			{
+				str >> tmp;
+
+				if ((tmp == UTF8("true")) || (tmp == UTF8("1"))
+					|| (tmp == UTF8("yes")) ||
+					(tmp == UTF8("1.0")))
+				{
+					value[i] = true;
+				}
+				else
+				{
+					value[i] = false;
+				}
+			}
+
+			return str;
+		}
+
+		template <typename T>
+		InStream& scan_mat(InStream &str, T &value)
+		{
+			Uint16 i, j;
+
+			for (i = 0; i < value.row_size(); ++i)
+			{
+				for (j = 0; j < value.col_size(); ++j)
+				{
+					str >> value[i][j];
+				}
+			}
+
+			return str;
+		}
+
+	}
+
 	OutStream& operator<<(OutStream &str, const glm::quat &value)
 	{
-		str << value.x;
-		str << L" " << value.y;
-		str << L" " << value.z;
-		str << L" " << value.w;
-
-		return str;
+		return print_quat(str, value);
 	}
 
 	OutStream& operator<<(OutStream &str, const glm::vec2 &value)
 	{
-		str << value[0];
-		str << L" " << value[1];
-
-		return str;
+		return print_vec(str, value);
 	}
 
 	OutStream& operator<<(OutStream &str, const glm::vec3 &value)
 	{
-		str << value[0];
-		str << L" " << value[1];
-		str << L" " << value[2];
-
-		return str;
+		return print_vec(str, value);
 	}
 
 	OutStream& operator<<(OutStream &str, const glm::vec4 &value)
 	{
-		str << value[0];
-		str << L" " << value[1];
-		str << L" " << value[2];
-		str << L" " << value[3];
-
-		return str;
+		return print_vec(str, value);
 	}
 
 	OutStream& operator<<(OutStream &str, const glm::bvec2 &value)
 	{
-		str << value[0];
-		str << L" " << value[1];
-
-		return str;
+		return print_bvec(str, value);
 	}
 
 	OutStream& operator<<(OutStream &str, const glm::bvec3 &value)
 	{
-		str << value[0];
-		str << L" " << value[1];
-		str << L" " << value[2];
-
-		return str;
+		return print_bvec(str, value);
 	}
 
 	OutStream& operator<<(OutStream &str, const glm::bvec4 &value)
 	{
-		str << value[0];
-		str << L" " << value[1];
-		str << L" " << value[2];
-		str << L" " << value[3];
-
-		return str;
+		return print_bvec(str, value);
 	}
 
 	OutStream& operator<<(OutStream &str, const glm::ivec2 &value)
 	{
-		str << value[0];
-		str << L" " << value[1];
-
-		return str;
+		return print_vec(str, value);
 	}
 
 	OutStream& operator<<(OutStream &str, const glm::ivec3 &value)
 	{
-		str << value[0];
-		str << L" " << value[1];
-		str << L" " << value[2];
-
-		return str;
+		return print_vec(str, value);
 	}
 
 	OutStream& operator<<(OutStream &str, const glm::ivec4 &value)
 	{
-		str << value[0];
-		str << L" " << value[1];
-		str << L" " << value[2];
-		str << L" " << value[3];
-
-		return str;
+		return print_vec(str, value);
 	}
 
 	OutStream& operator<<(OutStream &str, const glm::uvec2 &value)
 	{
-		str << value[0];
-		str << L" " << value[1];
-
-		return str;
+		return print_vec(str, value);
 	}
 
 	OutStream& operator<<(OutStream &str, const glm::uvec3 &value)
 	{
-		str << value[0];
-		str << L" " << value[1];
-		str << L" " << value[2];
-
-		return str;
+		return print_vec(str, value);
 	}
 
 	OutStream& operator<<(OutStream &str, const glm::uvec4 &value)
 	{
-		str << value[0];
-		str << L" " << value[1];
-		str << L" " << value[2];
-		str << L" " << value[3];
-
-		return str;
+		return print_vec(str, value);
 	}
 
 	OutStream& operator<<(OutStream &str, const glm::mat2x2 &value)
 	{
-		str << value[0][0];
-		str << L" " << value[0][1];
-		str << L" " << value[1][0];
-		str << L" " << value[1][1];
-
-		return str;
+		return print_mat(str, value);
 	}
 
 	OutStream& operator<<(OutStream &str, const glm::mat2x3 &value)
 	{
-		str << value[0][0];
-		str << L" " << value[0][1];
-		str << L" " << value[0][2];
-		str << L" " << value[1][0];
-		str << L" " << value[1][1];
-		str << L" " << value[1][2];
-
-		return str;
+		return print_mat(str, value);
 	}
 
 	OutStream& operator<<(OutStream &str, const glm::mat2x4 &value)
 	{
-		str << value[0][0];
-		str << L" " << value[0][1];
-		str << L" " << value[0][2];
-		str << L" " << value[0][3];
-		str << L" " << value[1][0];
-		str << L" " << value[1][1];
-		str << L" " << value[1][2];
-		str << L" " << value[1][3];
-
-		return str;
+		return print_mat(str, value);
 	}
 
 	OutStream& operator<<(OutStream &str, const glm::mat3x2 &value)
 	{
-		str << value[0][0];
-		str << L" " << value[0][1];
-		str << L" " << value[1][0];
-		str << L" " << value[1][1];
-		str << L" " << value[2][0];
-		str << L" " << value[2][1];
-
-		return str;
+		return print_mat(str, value);
 	}
 
 	OutStream& operator<<(OutStream &str, const glm::mat3x3 &value)
 	{
-		str << value[0][0];
-		str << L" " << value[0][1];
-		str << L" " << value[0][2];
-		str << L" " << value[1][0];
-		str << L" " << value[1][1];
-		str << L" " << value[1][2];
-		str << L" " << value[2][0];
-		str << L" " << value[2][1];
-		str << L" " << value[2][2];
-
-		return str;
+		return print_mat(str, value);
 	}
 
 	OutStream& operator<<(OutStream &str, const glm::mat3x4 &value)
 	{
-		str << value[0][0];
-		str << L" " << value[0][1];
-		str << L" " << value[0][2];
-		str << L" " << value[0][3];
-		str << L" " << value[1][0];
-		str << L" " << value[1][1];
-		str << L" " << value[1][2];
-		str << L" " << value[1][3];
-		str << L" " << value[2][0];
-		str << L" " << value[2][1];
-		str << L" " << value[2][2];
-		str << L" " << value[2][3];
-
-		return str;
+		return print_mat(str, value);
 	}
 
 	OutStream& operator<<(OutStream &str, const glm::mat4x2 &value)
 	{
-		str << value[0][0];
-		str << L" " << value[0][1];
-		str << L" " << value[1][0];
-		str << L" " << value[1][1];
-		str << L" " << value[2][0];
-		str << L" " << value[2][1];
-		str << L" " << value[3][0];
-		str << L" " << value[3][1];
-
-		return str;
+		return print_mat(str, value);
 	}
 
 	OutStream& operator<<(OutStream &str, const glm::mat4x3 &value)
 	{
-		str << value[0][0];
-		str << L" " << value[0][1];
-		str << L" " << value[0][2];
-		str << L" " << value[1][0];
-		str << L" " << value[1][1];
-		str << L" " << value[1][2];
-		str << L" " << value[2][0];
-		str << L" " << value[2][1];
-		str << L" " << value[2][2];
-		str << L" " << value[3][0];
-		str << L" " << value[3][1];
-		str << L" " << value[3][2];
-
-		return str;
+		return print_mat(str, value);
 	}
 
 	OutStream& operator<<(OutStream &str, const glm::mat4x4 &value)
 	{
-		str << value[0][0];
-		str << L" " << value[0][1];
-		str << L" " << value[0][2];
-		str << L" " << value[0][3];
-		str << L" " << value[1][0];
-		str << L" " << value[1][1];
-		str << L" " << value[1][2];
-		str << L" " << value[1][3];
-		str << L" " << value[2][0];
-		str << L" " << value[2][1];
-		str << L" " << value[2][2];
-		str << L" " << value[2][3];
-		str << L" " << value[3][0];
-		str << L" " << value[3][1];
-		str << L" " << value[3][2];
-		str << L" " << value[3][3];
-
-		return str;
+		return print_mat(str, value);
 	}
 
 	InStream& operator>>(InStream &str, glm::quat &value)
 	{
-		str >> value.x;
-		str >> value.y;
-		str >> value.z;
-		str >> value.w;
-
-		return str;
+		return scan_quat(str, value);
 	}
 
 	InStream& operator>>(InStream &str, glm::vec2 &value)
 	{
-		str >> value[0];
-		str >> value[1];
-
-		return str;
+		return scan_vec(str, value);
 	}
 
 	InStream& operator>>(InStream &str, glm::vec3 &value)
 	{
-		str >> value[0];
-		str >> value[1];
-		str >> value[2];
-
-		return str;
+		return scan_vec(str, value);
 	}
 
 	InStream& operator>>(InStream &str, glm::vec4 &value)
 	{
-		str >> value[0];
-		str >> value[1];
-		str >> value[2];
-		str >> value[3];
-
-		return str;
+		return scan_vec(str, value);
 	}
 
 	InStream& operator>>(InStream &str, glm::bvec2 &value)
 	{
-		str >> value[0];
-		str >> value[1];
-
-		return str;
+		return scan_bvec(str, value);
 	}
 
 	InStream& operator>>(InStream &str, glm::bvec3 &value)
 	{
-		str >> value[0];
-		str >> value[1];
-		str >> value[2];
-
-		return str;
+		return scan_bvec(str, value);
 	}
 
 	InStream& operator>>(InStream &str, glm::bvec4 &value)
 	{
-		str >> value[0];
-		str >> value[1];
-		str >> value[2];
-		str >> value[3];
-
-		return str;
+		return scan_bvec(str, value);
 	}
 
 	InStream& operator>>(InStream &str, glm::ivec2 &value)
 	{
-		str >> value[0];
-		str >> value[1];
-
-		return str;
+		return scan_vec(str, value);
 	}
 
 	InStream& operator>>(InStream &str, glm::ivec3 &value)
 	{
-		str >> value[0];
-		str >> value[1];
-		str >> value[2];
-
-		return str;
+		return scan_vec(str, value);
 	}
 
 	InStream& operator>>(InStream &str, glm::ivec4 &value)
 	{
-		str >> value[0];
-		str >> value[1];
-		str >> value[2];
-		str >> value[3];
-
-		return str;
+		return scan_vec(str, value);
 	}
 
 	InStream& operator>>(InStream &str, glm::uvec2 &value)
 	{
-		str >> value[0];
-		str >> value[1];
-
-		return str;
+		return scan_vec(str, value);
 	}
 
 	InStream& operator>>(InStream &str, glm::uvec3 &value)
 	{
-		str >> value[0];
-		str >> value[1];
-		str >> value[2];
-
-		return str;
+		return scan_vec(str, value);
 	}
 
 	InStream& operator>>(InStream &str, glm::uvec4 &value)
 	{
-		str >> value[0];
-		str >> value[1];
-		str >> value[2];
-		str >> value[3];
-
-		return str;
+		return scan_vec(str, value);
 	}
 
 	InStream& operator>>(InStream &str, glm::mat2x2 &value)
 	{
-		str >> value[0][0];
-		str >> value[0][1];
-		str >> value[1][0];
-		str >> value[1][1];
-
-		return str;
+		return scan_mat(str, value);
 	}
 
 	InStream& operator>>(InStream &str, glm::mat2x3 &value)
 	{
-		str >> value[0][0];
-		str >> value[0][1];
-		str >> value[0][2];
-		str >> value[1][0];
-		str >> value[1][1];
-		str >> value[1][2];
-
-		return str;
+		return scan_mat(str, value);
 	}
 
 	InStream& operator>>(InStream &str, glm::mat2x4 &value)
 	{
-		str >> value[0][0];
-		str >> value[0][1];
-		str >> value[0][2];
-		str >> value[0][3];
-		str >> value[1][0];
-		str >> value[1][1];
-		str >> value[1][2];
-		str >> value[1][3];
-
-		return str;
+		return scan_mat(str, value);
 	}
 
 	InStream& operator>>(InStream &str, glm::mat3x2 &value)
 	{
-		str >> value[0][0];
-		str >> value[0][1];
-		str >> value[1][0];
-		str >> value[1][1];
-		str >> value[2][0];
-		str >> value[2][1];
-
-		return str;
+		return scan_mat(str, value);
 	}
 
 	InStream& operator>>(InStream &str, glm::mat3x3 &value)
 	{
-		str >> value[0][0];
-		str >> value[0][1];
-		str >> value[0][2];
-		str >> value[1][0];
-		str >> value[1][1];
-		str >> value[1][2];
-		str >> value[2][0];
-		str >> value[2][1];
-		str >> value[2][2];
-
-		return str;
+		return scan_mat(str, value);
 	}
 
 	InStream& operator>>(InStream &str, glm::mat3x4 &value)
 	{
-		str >> value[0][0];
-		str >> value[0][1];
-		str >> value[0][2];
-		str >> value[0][3];
-		str >> value[1][0];
-		str >> value[1][1];
-		str >> value[1][2];
-		str >> value[1][3];
-		str >> value[2][0];
-		str >> value[2][1];
-		str >> value[2][2];
-		str >> value[2][3];
-
-		return str;
+		return scan_mat(str, value);
 	}
 
 	InStream& operator>>(InStream &str, glm::mat4x2 &value)
 	{
-		str >> value[0][0];
-		str >> value[0][1];
-		str >> value[1][0];
-		str >> value[1][1];
-		str >> value[2][0];
-		str >> value[2][1];
-		str >> value[3][0];
-		str >> value[3][1];
-
-		return str;
+		return scan_mat(str, value);
 	}
 
 	InStream& operator>>(InStream &str, glm::mat4x3 &value)
 	{
-		str >> value[0][0];
-		str >> value[0][1];
-		str >> value[0][2];
-		str >> value[1][0];
-		str >> value[1][1];
-		str >> value[1][2];
-		str >> value[2][0];
-		str >> value[2][1];
-		str >> value[2][2];
-		str >> value[3][0];
-		str >> value[3][1];
-		str >> value[3][2];
-
-		return str;
+		return scan_mat(str, value);
 	}
 
 	InStream& operator>>(InStream &str, glm::mat4x4 &value)
 	{
-		str >> value[0][0];
-		str >> value[0][1];
-		str >> value[0][2];
-		str >> value[0][3];
-		str >> value[1][0];
-		str >> value[1][1];
-		str >> value[1][2];
-		str >> value[1][3];
-		str >> value[2][0];
-		str >> value[2][1];
-		str >> value[2][2];
-		str >> value[2][3];
-		str >> value[3][0];
-		str >> value[3][1];
-		str >> value[3][2];
-		str >> value[3][3];
-
-		return str;
+		return scan_mat(str, value);
 	}
 
 }
---
+++ tests/default/glmutil.cpp
@@ -0,0 +1,201 @@
+/****************************************************************************
+ *            glmutil.cpp
+ *
+ * Author: 2011  Daniel Jungmann <el.3d.source@googlemail.com>
+ * Copyright: See COPYING file that comes with this distribution
+ ****************************************************************************/
+
+#include "prerequisites.hpp"
+#include "glmutil.hpp"
+#include <boost/random.hpp>
+#define BOOST_TEST_MODULE glmutil
+#include <boost/test/unit_test.hpp>
+
+using namespace eternal_lands;
+
+BOOST_AUTO_TEST_CASE(quat_in_out)
+{
+	boost::mt19937 rng;
+	boost::uniform_int<Sint32> range(-16777216, 16777216);
+	boost::variate_generator<boost::mt19937&, boost::uniform_int<Sint32> >
+		random_int(rng, range);
+	glm::quat tmp0, tmp1;
+	Uint16 i, j;
+
+	for (i = 0; i < std::numeric_limits<Uint16>::max(); ++i)
+	{
+		StringStream str;
+
+		for (j = 0; j < 4; ++j)
+		{
+			tmp0[j] = random_int() * 0.01f;
+		}
+
+		BOOST_CHECK_NO_THROW(str << tmp0);
+		BOOST_CHECK_NO_THROW(str >> tmp1);
+
+		for (j = 0; j < 4; ++j)
+		{
+			BOOST_CHECK_CLOSE(tmp1[j], tmp0[j], 0.001);
+		}
+	}
+}
+
+typedef boost::mpl::list<glm::vec2, glm::vec3, glm::vec4> vec_types;
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(vec_in_out, T, vec_types)
+{
+	boost::mt19937 rng;
+	boost::uniform_int<Sint32> range(-16777216, 16777216);
+	boost::variate_generator<boost::mt19937&, boost::uniform_int<Sint32> >
+		random_int(rng, range);
+	T tmp0, tmp1;
+	Uint16 i, j;
+
+	for (i = 0; i < std::numeric_limits<Uint16>::max(); ++i)
+	{
+		StringStream str;
+
+		for (j = 0; j < tmp0.length(); ++j)
+		{
+			tmp0[j] = random_int() * 0.01f;
+		}
+
+		BOOST_CHECK_NO_THROW(str << tmp0);
+		BOOST_CHECK_NO_THROW(str >> tmp1);
+
+		for (j = 0; j < tmp0.length(); ++j)
+		{
+			BOOST_CHECK_CLOSE(tmp1[j], tmp0[j], 0.001);
+		}
+	}
+}
+
+typedef boost::mpl::list<glm::ivec2, glm::ivec3, glm::ivec4> ivec_types;
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(ivec_in_out, T, ivec_types)
+{
+	boost::mt19937 rng;
+	boost::uniform_int<Sint32> range(-16777216, 16777216);
+	boost::variate_generator<boost::mt19937&, boost::uniform_int<Sint32> >
+		random_int(rng, range);
+	T tmp0, tmp1;
+	Uint16 i, j;
+
+	for (i = 0; i < std::numeric_limits<Uint16>::max(); ++i)
+	{
+		StringStream str;
+
+		for (j = 0; j < tmp0.length(); ++j)
+		{
+			tmp0[j] = random_int();
+		}
+
+		BOOST_CHECK_NO_THROW(str << tmp0);
+		BOOST_CHECK_NO_THROW(str >> tmp1);
+
+		for (j = 0; j < tmp0.length(); ++j)
+		{
+			BOOST_CHECK_EQUAL(tmp1[j], tmp0[j]);
+		}
+	}
+}
+
+typedef boost::mpl::list<glm::uvec2, glm::uvec3, glm::uvec4> uvec_types;
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(uvec_in_out, T, uvec_types)
+{
+	boost::mt19937 rng;
+	boost::uniform_int<Uint32> range(0, 16777216);
+	boost::variate_generator<boost::mt19937&, boost::uniform_int<Uint32> >
+		random_uint(rng, range);
+	T tmp0, tmp1;
+	Uint16 i, j;
+
+	for (i = 0; i < std::numeric_limits<Uint16>::max(); ++i)
+	{
+		StringStream str;
+
+		for (j = 0; j < tmp0.length(); ++j)
+		{
+			tmp0[j] = random_uint();
+		}
+
+		BOOST_CHECK_NO_THROW(str << tmp0);
+		BOOST_CHECK_NO_THROW(str >> tmp1);
+
+		for (j = 0; j < tmp0.length(); ++j)
+		{
+			BOOST_CHECK_EQUAL(tmp1[j], tmp0[j]);
+		}
+	}
+}
+
+typedef boost::mpl::list<glm::bvec2, glm::bvec3, glm::bvec4> bvec_types;
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(bvec_in_out, T, bvec_types)
+{
+	boost::mt19937 rng;
+	boost::uniform_int<Uint32> range(0, 1);
+	boost::variate_generator<boost::mt19937&, boost::uniform_int<Uint32> >
+		random_uint(rng, range);
+	T tmp0, tmp1;
+	Uint16 i, j;
+
+	for (i = 0; i < std::numeric_limits<Uint16>::max(); ++i)
+	{
+		StringStream str;
+
+		for (j = 0; j < tmp0.length(); ++j)
+		{
+			tmp0[j] = random_uint() == 0;
+		}
+
+		BOOST_CHECK_NO_THROW(str << tmp0);
+		BOOST_CHECK_NO_THROW(str >> tmp1);
+
+		for (j = 0; j < tmp0.length(); ++j)
+		{
+			BOOST_CHECK_EQUAL(tmp1[j], tmp0[j]);
+		}
+	}
+}
+
+typedef boost::mpl::list<glm::mat2x2, glm::mat2x3, glm::mat2x4, glm::mat3x2,
+	glm::mat3x3, glm::mat3x4, glm::mat4x2, glm::mat4x3, glm::mat4x4>
+	mat_types;
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(mat_in_out, T, mat_types)
+{
+	boost::mt19937 rng;
+	boost::uniform_int<Sint32> range(-16777216, 16777216);
+	boost::variate_generator<boost::mt19937&, boost::uniform_int<Sint32> >
+		random_int(rng, range);
+	T tmp0, tmp1;
+	Uint16 i, j, k;
+
+	for (i = 0; i < std::numeric_limits<Uint16>::max(); ++i)
+	{
+		StringStream str;
+
+		for (j = 0; j < tmp0.row_size(); ++j)
+		{
+			for (k = 0; k < tmp0.col_size(); ++k)
+			{
+				tmp0[j][k] = random_int() * 0.01f;
+			}
+		}
+
+		BOOST_CHECK_NO_THROW(str << tmp0);
+		BOOST_CHECK_NO_THROW(str >> tmp1);
+
+		for (j = 0; j < tmp0.length(); ++j)
+		{
+			for (k = 0; k < tmp0.col_size(); ++k)
+			{
+				BOOST_CHECK_CLOSE(tmp1[j][k], tmp0[j][k],
+					0.001);
+			}
+		}
+	}
+}
